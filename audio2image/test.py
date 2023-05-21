c5da22ccb1438bec01277050f94496e2cc4d8e01
Tianchong Jiang <tianchongjiang@gmail.com>
2023-05-21 14:32:17 -0500

    added some scripts

--- audio2image/test.py
+++ audio2image/test.py
@@ -4,10 +4,11 @@ import pdb
 
 inferece = vd_inference(which='v1.0', fp16=True)
 
-# load image
-image = Image.open("/rmx/diffuser/input.png")
+image = Image.open("/rmx/diffuser/code/image.png")
 
 text, latent_code = inferece.inference_i2l(image, 1234)
+
+# latent_code = latent_code[0].repeat(77, 1)
 
 output_image = inferece.inference_l2i(latent_code, 1234)
 
